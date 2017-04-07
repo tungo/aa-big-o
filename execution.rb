69cc30e1fe935176308d30fc35d33bcded4fa611
Tu Ngo <tungo8810@gmail.com>
2017-04-06 17:53:11 -0700

    Refactor excutions

--- execution.rb
+++ execution.rb
@@ -1,42 +1,37 @@
-def my_min arr
-  min = nil
-  arr.each.with_index do |el, i|
-    (el...arr.length).each do |j|
-      min = arr[j] if el < arr[j]
+def my_min(arr)
+  arr.each.with_index do |el, idx1|
+    min = true
+    (idx1 + 1...arr.length).each do |idx2|
+      min = false if el > arr[idx2]
     end
+
+    return el if min
   end
+
+  nil
 end
 
-def my_min_II arr
+def my_min2(arr)
   min = nil
-  arr.each {|el| min = el if (min == nil || min > el)}
+  arr.each { |el| min = el if min == nil || min > el }
   min
 end
 
-def power_set arr
-  arr.reduce([[]]) {|powerset, el|
-    powerset + powerset.map{|set| [el] + set}
-  }
-end
-
-def sub_sum arr
-  power_set(arr).map do |set|
-    set.reduce(0, &:+)
-  end.max
+def sub_sum(arr)
+  
 end
 
 def sub_sum2(arr)
   max_so_far = nil
   max_ending_here = 0
 
-  arr.each_with_index do |el, i|
+  arr.each do |el|
     max_ending_here += el
-    p "max_ending_here == " + max_ending_here.to_s
-    p "max_so_far == " + max_so_far.to_s
 
     if max_so_far.nil? || max_so_far < max_ending_here
       max_so_far = max_ending_here
     end
+
     max_ending_here = 0 if max_ending_here < 0
   end
 
